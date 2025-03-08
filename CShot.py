cae14b497609d2ab24e9d5d5a20960b15032bc4e
ILIA <ih13843251@gmail.com>
2025-03-08 06:35:24 -0800

    screen

--- CShot.py
+++ CShot.py
@@ -1,1 +1,15 @@
 import pygame
+from sys import exit
+
+pygame.init()
+pygame.display.set_mode((800,500))
+pygame.display.set_caption("CShot")
+ 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    
+    pygame.display.update()
