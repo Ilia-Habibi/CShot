63a2334d9d78f41188d587c27f649981ba635234
Ilia-Habibi <ih13843251@gmail.com>
2025-03-10 10:34:53 +0330

    icon and max fps

--- CShot.py
+++ CShot.py
@@ -1,10 +1,15 @@
 import pygame
 from sys import exit
 
+# Initializing pygame and creaing a screen
 pygame.init()
 pygame.display.set_mode((800,500))
 pygame.display.set_caption("CShot")
- 
+clock = pygame.time.Clock()
+icon = pygame.image.load('Graphics/cshot icon.png')
+pygame.display.set_icon(icon)
+
+    
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,3 +18,4 @@ while True:
 
     
     pygame.display.update()
+    clock.tick(60)
