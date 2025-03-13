4fb09b5c997c7fb512c3222836330e6c82ece258
ILIA <ih13843251@gmail.com>
2025-03-13 01:03:28 -0700

    background and random aim

--- CShot.py
+++ CShot.py
@@ -1,13 +1,34 @@
-import pygame
+import pygame,random
 from sys import exit
+
+class PLAYER:
+    def __init__(self):
+        self.randomize_aim()
+
+    def randomize_aim(self):
+        self.x = random.randint(20,780)
+        self.y = random.randint(80,480)
+        self.aim = pygame.math.Vector2(self.x,self.y)
+
+class MAIN:
+    def __init__(self):
+        self.BG = pygame.image.load('Graphics/game BG.jpg')
+
+    def draw(self):
+        self.draw_game_BG()
+
+    def draw_game_BG(self):
+        screen.blit(self.BG,(0,0))
+
 
 # Initializing pygame and creaing a screen
 pygame.init()
-pygame.display.set_mode((800,500))
+screen = pygame.display.set_mode((800,500))
 pygame.display.set_caption("CShot")
 clock = pygame.time.Clock()
 icon = pygame.image.load('Graphics/cshot icon.png')
 pygame.display.set_icon(icon)
+main = MAIN()
 
     
 while True:
@@ -17,5 +38,6 @@ while True:
             exit()
 
     
+    main.draw()
     pygame.display.update()
     clock.tick(60)
