353b84cb74c3f24845015f07efe00c5769794d75
ILIA <ih13843251@gmail.com>
2025-03-15 07:37:11 -0700

    deleting extra stuff

--- CShot.py
+++ CShot.py
@@ -1,6 +1,5 @@
 import pygame,random
-import sys
-from Basic_options import *
+from sys import exit
 
 class PLAYER:
     def __init__(self,player_no):
@@ -23,38 +22,58 @@ class MAIN:
         self.player1 = PLAYER(1)
         self.player2 = PLAYER(2)
         ### page numbers: 0 = menu , 1 = name input , 2 = game , 3 = controls , 4 = leaderboard
-        self.page = 0
+        self.page = 2
 
-    def draw_game_jpg(self):
+    
+    def draw_BG(self):
         screen.blit(self.BG, (0,0))
 
     def draw_game(self):
-        self.draw_game_jpg() 
+        self.draw_BG()
         self.player1.draw_shot()
         self.player2.draw_shot()  
 
-    def draw_menu(self):
-        pass
-
-screen = pygame.display.set_mode(res)
+    
+# Initializing pygame and creating a screen
+pygame.init()
+width = 820
+height = 500
+screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("CShot")
 clock = pygame.time.Clock()
 icon = pygame.image.load('Graphics/cshot icon.png')
 pygame.display.set_icon(icon)
+
+#fonts
+font1 = pygame.font.Font('Fonts/WSBH.ttf',30)
+
 main = MAIN()
     
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                main.player1.shot_bullets.append(main.player1.aim.copy()) 
-            if event.key == pygame.K_SPACE:
-                main.player2.shot_bullets.append(main.player2.aim.copy())
+            exit()
 
-    main.draw_game()
+        if main.page == 0:
+            pass
+        elif main.page == 1:
+            pass
+        elif main.page == 2: 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    main.player1.shot_bullets.append(main.player1.aim.copy()) 
+                if event.key == pygame.K_SPACE:
+                    main.player2.shot_bullets.append(main.player2.aim.copy())
+            
+            main.draw_game()
+            
+        elif main.page == 3:
+            pass
+        elif main.page == 4:
+            pass
+
+
     pygame.display.update()
     clock.tick(60)
 
