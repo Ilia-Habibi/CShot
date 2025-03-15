e327cc55861281a93f4e470b69504d1b72d313a3
ILIA <ih13843251@gmail.com>
2025-03-15 02:03:40 -0700

    bullet shooting

--- CShot.py
+++ CShot.py
@@ -6,14 +6,16 @@ class PLAYER:
     def __init__(self,player_no):
         self.randomize_aim()
         self.bullet = pygame.image.load('Graphics/red.png') if player_no == 1 else pygame.image.load('Graphics/blue.png')
+        self.shot_bullets = []
 
     def randomize_aim(self):
         self.x = random.randint(20,800)
         self.y = random.randint(80,480)
         self.aim = pygame.math.Vector2(self.x,self.y)
     
-    def shot(self):
-        screen.blit(self.bullet,self.aim)
+    def draw_shot(self):
+        for i in self.shot_bullets:
+            screen.blit(self.bullet,i)
 
 class MAIN:
     def __init__(self):
@@ -23,6 +25,8 @@ class MAIN:
 
     def draw(self):
         self.draw_game_BG()
+        self.player1.draw_shot()
+        self.player2.draw_shot()
 
     def draw_game_BG(self):
         screen.blit(self.BG,(0,0))
@@ -36,7 +40,14 @@ def menu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:  
-                    pygame.quit()  
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    main.player1.shot_bullets.append(main.player1.aim.copy()) 
+                if event.key == pygame.K_SPACE:
+                    main.player2.shot_bullets.append(main.player2.aim.copy())
+
             main.draw()
             mouse = pygame.mouse.get_pos()
             screen.blit(text , (width/2+50,height/2))   
